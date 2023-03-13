67b46449ac3ff3407dae034e2688a81a2c59c8c7
dariia-zhukovska <dariia.zhukovska@gmail.com>
2023-03-13 23:37:01 +0200

    portfolio

--- _assets/js/common/tabs.js
+++ _assets/js/common/tabs.js
@@ -31,5 +31,31 @@ export default () => {
       inactiveTab.style.color = 'grey';
     });
   });
-
 }
+
+
+
+  // export default () => {
+  //   document.querySelectorAll('.tabs__btn').forEach((btn) => {
+  //     btn.addEventListener('click', (event) => {
+
+  //       document.querySelectorAll('.tabs__btn').forEach((tabBtn) => {
+  //         tabBtn.classList.remove('tab-content-active');
+  //         tabBtn.style.color = 'grey';
+  //         // add this line to make both buttons black
+  //         tabBtn.style.color = 'black';
+  //       });
+  //       const path = event.currentTarget.dataset.path;
+  //       document.querySelectorAll('.tab-content').forEach((tabContent) => {
+  //         tabContent.classList.remove('tab-content-active');
+  //       });
+
+  //       document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
+  //       event.currentTarget.style.color = 'black';
+
+  //       const inactiveTab = document.querySelector(`.tab-content:not([data-target="${path}"])`);
+  //       inactiveTab.style.color = 'grey';
+  //     });
+  //   });
+  // }
+
